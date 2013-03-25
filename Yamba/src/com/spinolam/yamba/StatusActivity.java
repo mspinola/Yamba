0f9412d1d6c863f27b5e206dac55bdceaef4d576
Matthew Spinola <matt.spinola@gmail.com>
2013-03-24 22:06:10 -0400

    updated to add internet permission added status activity source code

--- Yamba/src/com/spinolam/yamba/StatusActivity.java
+++ Yamba/src/com/spinolam/yamba/StatusActivity.java
@@ -1,22 +1,44 @@
 package com.spinolam.yamba;
 
-import android.os.Bundle;
+import winterwell.jtwitter.Twitter;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.view.Menu;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	private static final String TAG ="StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
+		
+		// Find views
+		editText = (EditText) findViewById(R.id.editText);
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+		updateButton.setOnClickListener(this);
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
 	}
 
+	// Called when button is clicked 
+	public void onClick(View v) {
+		twitter.setStatus(editText.getText().toString());
+		Log.d(TAG, "onClicked");
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
 	}
-
 }
